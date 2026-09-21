4e04a56961b68f7b046774a2797f6b4193a6f4ed
LAPTOP-PSJ9CUPN\hugob <hugo.boutroue02@gmail.com>
2026-09-21 14:44:11 +0200

    feature: adding the routing, related views, page title

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
