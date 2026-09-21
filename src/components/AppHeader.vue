09c69b4e09b1ec03454b63730959eb99024c61ef
LAPTOP-PSJ9CUPN\hugob <hugo.boutroue02@gmail.com>
2026-09-21 15:05:30 +0200

    feature: adding app header, removing vue default styling,

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"> 
+</script>
+
+<template>
+    <header class="p-3 text-bg-dark">
+  <div class="container">
+    <div
+      class="d-flex flex-wrap align-items-center justify-content-center"
+    >
+      <RouterLink
+        to="/"
+        class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+      >
+      Purple Cross Ltd
+      </RouterLink>
+
+      <div class="text-end">
+        Hello, User
+      </div>
+    </div>
+  </div>
+</header>
+</template>
