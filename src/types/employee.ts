19b29adf00852fef99dca939694e0b6f0c34e6ed
LAPTOP-PSJ9CUPN\hugob <hugo.boutroue02@gmail.com>
2026-09-21 13:00:14 +0200

    core: add employee data model

---
+++ src/types/employee.ts
@@ -0,0 +1,17 @@
+export interface EmployeeInput {
+    code: string
+    fullName: string
+    occupation: string
+    department: string
+    dateOfEmployment: string
+    terminationDate: string | null
+}
+
+// Used for saving the record. Seperates data concern.
+export interface Employee extends EmployeeInput {
+    id: string
+}
+
+export type EmploymentStatus = 'Employed soon' | 'Currently employed'
+export type TerminationStatus = 'To be terminated' | 'Terminated' | null
+
