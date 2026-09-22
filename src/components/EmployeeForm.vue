1dcb0c641d3d2e1fdc109cbe2145785df1e49c5a
LAPTOP-PSJ9CUPN\hugob <hugo.boutroue02@gmail.com>
2026-09-22 10:54:35 +0200

    chore: removing unused property

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { reactive } from 'vue'
 import { CalendarDays, Check, UserRound } from '@lucide/vue'
 import { useEmployeeStore } from '../stores/employeeStore'
 import type { Employee, EmployeeInput } from '../types/employee'
